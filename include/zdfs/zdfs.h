6e0fefca590da9baf6bb4360681f20832a27dc58
jerome-trc <46534306+jerome-trc@users.noreply.github.com>
2024-07-11 10:48:20 -0400

    Begin documenting C interface via Doxygen

--- include/zdfs/zdfs.h
+++ include/zdfs/zdfs.h
@@ -19,10 +19,13 @@ extern "C" {
 typedef int32_t zdfs_WadNum;
 typedef int32_t zdfs_LumpNum;
 
+/// @see zdfs_fs_new
 typedef struct zdfs_FileSys zdfs_FileSys;
 
+/// @brief Destroys a filesystem instance.
 void zdfs_fs_free(const zdfs_FileSys* const);
 
+/// @returns A pointer to an empty filesystem instance allocated on the heap.
 ZDFS_NODISCARD zdfs_FileSys* zdfs_fs_new(void);
 
 #if defined(__cplusplus)
